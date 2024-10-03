f22a6e4ca17415a6f96d15edb0bb7185cbd6fa31
alexlegreat <yabrofaj@mail.uc.edu>
2024-10-03 14:15:38 -0400

    complete

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int summerCost = 4;
+        int fallCost = 1;
+        int winterCost = 6;
+        int springCost = 8;
+        int totalCost = 0;
+
+        totalCost = summerCost + fallCost + winterCost + springCost;
+        System.out.println("Total cost: $" + totalCost);
+    }
+}
